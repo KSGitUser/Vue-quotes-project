3acbfe9e5b4b3930ddf8bfe44705114ea1450945
KSFirstUser <rusakk@mail.ru>
2019-07-20 23:07:38 +0300

    Finished project

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on quote to delete it!</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import QuoteGrid from "./components/QuoteGrid.vue";
 import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
 
 export default {
     components: {
         appQuoteGrid: QuoteGrid,
-        appNewQuote: NewQuote
+        appNewQuote: NewQuote,
+        appHeader: Header
     },
     data() {
         return {
@@ -22,7 +30,15 @@ export default {
     },
     methods: {
         newQuote(quote) {
+            if (this.quotes.length >= this.maxQuotes) {
+                return alert(
+                    `Do not add quotes anymore. I'm too tired. Delete some quotes before!`
+                );
+            }
             this.quotes.push(quote);
+        },
+        deleteQuote(index) {
+            this.quotes.splice(index, 1);
         }
     }
 };
